bf25080b8032125132c076f177a3cab66ed1cd18
jak-ket <mynpmail@gmx.de>
2024-07-20 18:29:18 +0200

    visualize RFR Correlation among Region Pairs

--- yield_curve_analysis.R
+++ yield_curve_analysis.R
@@ -4,7 +4,10 @@ library(readxl)
 library(ggplot2)
 library(viridis)
 
+# create data directory
+
 rfr_data_path <- "data/rfr/"
+dir.create(rfr_data_path, recursive = T)
 
 # Download data
 
@@ -107,6 +110,8 @@ stopifnot(all(df[,cols.rates]>-0.01) & all(df[,cols.rates] < 1))
 df[which(df<0, arr.ind = T)] # check negative yields
 max(df[cols.rates])
 
+# save dataframe
+save(df, file=paste0(rfr_data_path, "rfr.Rda"))
 
 # Visualize the changes in US and Euro yield curve across tenors since 2022
 plot_yield_curves <- function(region){
@@ -122,3 +127,47 @@ plot_yield_curves <- function(region){
 }
 plot_yield_curves("Euro")
 plot_yield_curves("United.States")
+
+# Compute and visualise the correlation between US/Euro/UK/China rates focusing only on 2/5/10/20/30y tenor points
+
+# computation of data frame
+colnames(df)
+tenors <- c(2,5,10,20,30)
+regions <- c("United.States", "Euro", "United.Kingdom", "China")
+
+df[df$Tenor==5, "United.States"]
+
+region.pairs <- combn(regions,2)
+# df_cors <- data.frame(matrix(NA, nrow=length(tenors), ncol=dim(region.pairs)[2]))
+
+df_cors <- data.frame()
+
+for (i in 1:dim(region.pairs)[2]){
+  region1 <- region.pairs[1,i]
+  region2 <- region.pairs[2,i]
+  
+  name.region.pair <- paste(region1, "/", region2)
+  
+  for(j in 1:length(tenors)){
+    tenor <- tenors[j]
+    rfr1 = df[df$Tenor==tenor, region1]
+    rfr2 = df[df$Tenor==tenor, region2]
+    cor_region_tenor <- cor(rfr1, rfr2)
+    df_cors <- rbind(df_cors, c(name.region.pair, tenor, cor_region_tenor))
+  }
+}
+colnames(df_cors) <- c("Region.Pair", "Tenor", "Correlation")
+df_cors$Correlation <- as.numeric(df_cors$Correlation)
+df_cors$Tenor <- as.numeric(df_cors$Tenor)
+df_cors
+
+# visualization of correlations across tenors
+ggplot(df_cors, aes(x=Tenor, y=Correlation, color=as.factor(Region.Pair))) + 
+  geom_line(aes(color = as.factor(Region.Pair)), size = 1) + 
+  scale_color_viridis_d() +
+  scale_x_continuous("Maturity (Years)", labels = as.character(tenors), breaks = tenors) +
+  labs(title = "RFR Correlation among Region Pairs",
+       y = "Correlation",
+       color = "Region Pair") +
+  theme_minimal() +
+  theme(legend.position = "bottom")
